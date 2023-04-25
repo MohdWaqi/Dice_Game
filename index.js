bed0f9083d1100c094c3ded67e0633d4449f0422
Mohd Waqi Pervez <mohdwaqipervez@gmail.com>
2023-04-25 16:49:29 +0530

    add assets folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var player1 = Math.floor( ( Math.random() * 6 ) + 1 );
-document.getElementsByTagName("img")[0].setAttribute("src", "images/dice" + player1 + ".png");
+document.getElementsByTagName("img")[0].setAttribute("src", "assets/images/dice" + player1 + ".png");
 var player2 = Math.floor( ( Math.random() * 6 ) + 1 );
-document.getElementsByTagName("img")[1].setAttribute("src", "images/dice" + player2 + ".png");
+document.getElementsByTagName("img")[1].setAttribute("src", "assets/images/dice" + player2 + ".png");
 
 if (player1 > player2) {
     document.querySelector("h1").textContent = "🏆 Player 1 Wins";
